5b58af12ca386375af9e79b12c33779521928683
rida kamal <ridkam@utu.fi>
2021-10-19 12:46:00 +0300

    Initial commit

---
+++ dogsYear.js
@@ -0,0 +1,10 @@
+//early two years of dos year. in early two years of dog , are counted as 10.5 years each,
+// so early total early year will be 2 x 10.5
+let earlyYears= Math.floor(2*10.5);
+let myAge= 28;
+//subtracting early two dogs years from my age
+let YearsLater= myAge-2;
+//afer two years of dos years, rest of all the years are counted as 4 years, therefore
+let myYearsLater= YearsLater*4;
+let myYearInDogsYear = myYearsLater+earlyYears;
+console.log(myYearInDogsYear);
